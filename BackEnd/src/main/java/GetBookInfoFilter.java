833db78baefde8323db3ca1939b417c099b03d6a
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-03 20:22:35 +0800

    changed http get params

--- BackEnd/src/main/java/GetBookInfoFilter.java
+++ BackEnd/src/main/java/GetBookInfoFilter.java
@@ -1,8 +1,5 @@
-import org.json.JSONObject;
-
 import javax.servlet.*;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 public class GetBookInfoFilter implements Filter {
     @Override
@@ -12,9 +9,7 @@ public class GetBookInfoFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-        String reqStr = req.getReader().lines().collect(Collectors.joining());
-        JSONObject reqObject = new JSONObject(reqStr);
-        req.setAttribute("bookid", reqObject.getInt("book_id"));
+        req.setAttribute("bookid", Integer.parseInt(req.getParameter("book_id")));
         chain.doFilter(req, res);
     }
     @Override
