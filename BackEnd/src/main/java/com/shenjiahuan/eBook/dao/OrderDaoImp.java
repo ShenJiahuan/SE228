2c1a724f9626bdff55216346fd7125e7e9b3cbe1
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-18 16:24:45 +0800

    query modified for OrderDaoImp

--- BackEnd/src/main/java/com/shenjiahuan/eBook/dao/OrderDaoImp.java
+++ BackEnd/src/main/java/com/shenjiahuan/eBook/dao/OrderDaoImp.java
@@ -1,56 +1,50 @@
 package com.shenjiahuan.eBook.dao;
 
+import com.shenjiahuan.eBook.entity.Book;
 import com.shenjiahuan.eBook.entity.Order;
+import com.shenjiahuan.eBook.util.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class OrderDaoImp implements OrderDao {
 
-    private static SessionFactory sessionFactory;
-    static {
-        try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
     public List<Object> findOrderByUserId(int userId, boolean paid) {
-        Session session = sessionFactory.openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
 //        criteriaQuery.orderBy(builder.asc(order.get("purchase_time")));
-        String hql = "From Order A, Book B where A.bookId = B.bookId and A.uid = :uid and A.purchased = :paid";
+        String hql =
+                "From Order A, Book B " +
+                "where A.bookId = B.bookId and A.uid = :uid and A.purchased = :paid " +
+                "order by A.purchaseTime desc";
         Query query = session.createQuery(hql);
         query.setParameter("uid", userId);
         query.setParameter("paid", paid ? (byte) 1 : (byte) 0);
-        List<Object> orders = query.getResultList();
-        session.close();
+        @SuppressWarnings("unchecked")
+        List<Object> orders = query.list();
+        session.getTransaction().commit();
         return orders.size() != 0 ? orders : null;
     }
 
     public void createOrder(List<Order> orders) {
-        Session session = sessionFactory.openSession();
-        Transaction tx = null;
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
         try {
-            tx = session.beginTransaction();
             for (Order order : orders) {
                 session.save(order);
             }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+            ex.printStackTrace();
         }
+
     }
 }
