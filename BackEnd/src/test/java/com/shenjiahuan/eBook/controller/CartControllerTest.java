865ec67baf3e4b1bc55531caadcb89639baeae23
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-05-19 14:45:37 +0800

    added more tests

---
+++ BackEnd/src/test/java/com/shenjiahuan/eBook/controller/CartControllerTest.java
@@ -0,0 +1,62 @@
+package com.shenjiahuan.eBook.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class CartControllerTest {
+
+    @Autowired
+    CartController cartController;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(cartController).isNotNull();
+    }
+
+    @Test
+    public void getCartListUnauthorized() throws Exception {
+        this.mockMvc.perform(get("/cart"))
+                .andExpect(status().is(401));
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"NORMAL"})
+    public void getCartListSuccess() throws Exception {
+        this.mockMvc.perform(get("/cart"))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void addCart() {
+    }
+
+    @Test
+    public void updateCart() {
+    }
+
+    @Test
+    public void deleteCart() {
+    }
+}
