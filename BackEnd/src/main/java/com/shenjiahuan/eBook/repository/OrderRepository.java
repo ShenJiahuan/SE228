ec66c8452aa7b6a4a35aad95cdf1a7d47e4c2e4a
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-06-25 19:50:32 +0800

    sold count logic modify

--- BackEnd/src/main/java/com/shenjiahuan/eBook/repository/OrderRepository.java
+++ BackEnd/src/main/java/com/shenjiahuan/eBook/repository/OrderRepository.java
@@ -12,6 +12,10 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findOrdersByUidOrderByPayTimeDesc(int userId);
 
-    @Query("select oi.snapshot, sum(oi.count) from Order o inner join OrderItem oi on o.orderId = oi.order.orderId where o.payTime > ?1 and o.payTime < ?2 group by oi.snapshot order by sum(oi.count) desc")
+    @Query("select bs, sum(oi.count) " +
+           "from Order o inner join OrderItem oi on o.orderId = oi.order.orderId inner join BookSnapshot bs on oi.snapshot.bookId = bs.bookId " +
+           "where o.payTime > ?1 and o.payTime < ?2 and bs.snapTime >= all(select snapTime from BookSnapshot bs1 where bs.bookId = bs1.bookId)" +
+           "group by bs " +
+           "order by sum(oi.count) desc")
     List<Object> getStatus(BigDecimal from, BigDecimal to);
 }
